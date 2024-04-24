c58aa6cd8d4a825f66684621bbd9cf60adea2a64
Michael Petersen <michael@smoerfugl.dk>
2024-04-24 12:28:53 +0200

    Adding solution to repo

---
+++ DogRallyManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DogRallyManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
