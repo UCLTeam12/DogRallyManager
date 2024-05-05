ae13c51efbe106e4b09e8772259b25e42c7dd4c3
Michael Petersen <michael@smoerfugl.dk>
2024-05-05 14:20:30 +0200

    Code cleanup

--- DogRallyManager/DogRallyManager/Controllers/AccountController.cs
+++ DogRallyManager/DogRallyManager/Controllers/AccountController.cs
@@ -6,128 +6,123 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DogRallyManager.Controllers;
 
-    public class AccountController : Controller
+public class AccountController : Controller
+{
+    private readonly SignInManager<RallyUser> _signInManager;
+    private readonly UserManager<RallyUser> _userManager;
+    public RegisterUserVM RegisterUserVM = new();
+    public LoginUserVM LoginUserVM = new();
+
+    public AccountController(
+        SignInManager<RallyUser> signManager,
+        UserManager<RallyUser> userManager
+    )
     {
-        private readonly SignInManager<RallyUser> _signInManager;
-        private readonly UserManager<RallyUser> _userManager;
-        public RegisterUserVM RegisterUserVM = new();
-        public LoginUserVM LoginUserVM = new();
+        _signInManager = signManager;
+        _userManager = userManager;
+    }
 
-        public AccountController(
-            SignInManager<RallyUser> signManager,
-            UserManager<RallyUser> userManager
-            )
-        {
-            _signInManager = signManager;
-            _userManager = userManager;
+    // POSSIBLE TO-DO:
+    // Concider changing name of Index method to login, and returning view(). 
+    // This will require a rerouting on paths 
+    public async Task<IActionResult> Index()
+    {
+        await OnPostLogoutAsync();
+        return View("Login");
+    }
 
-        }
-        // POSSIBLE TO-DO:
-        // Concider changing name of Index method to login, and returning view(). 
-        // This will require a rerouting on paths 
-        public async Task<IActionResult> Index()
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View(RegisterUserVM);
+    }
+
+    public async Task<IActionResult> Logout(string choice)
+    {
+        if (choice == "yes")
         {
             await OnPostLogoutAsync();
-            return View("Login");
+            return RedirectToAction("Index"); // Redirect to home after logout
         }
 
-        [HttpGet]
-        public IActionResult Register()
+        if (choice == "no")
         {
-            return View(RegisterUserVM);
+            return OnPostDontLogout();
         }
-
-
-        public async Task<IActionResult> Logout(string choice)
+        else
         {
-            if (choice == "yes")
+            return View("Logout");
+        }
+    }
+
+    [Authorize]
+    public IActionResult YourPage()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> OnPostLogoutAsync()
+    {
+        // ASP NET will handle all the signout functions for us
+        await _signInManager.SignOutAsync();
+
+        return RedirectToAction("Login");
+    }
+
+    [HttpPost]
+    public IActionResult OnPostDontLogout()
+    {
+        return RedirectToAction("YourPage");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginUserVM LoginUserVM)
+    {
+        if (ModelState.IsValid)
+        {
+            var identityResult = await _signInManager.PasswordSignInAsync(
+                LoginUserVM.Email, LoginUserVM.Password, LoginUserVM.RememberMe, false);
+
+            if (identityResult.Succeeded)
             {
-                await OnPostLogoutAsync();
-                return RedirectToAction("Index"); // Redirect to home after logout
-            }
-            if (choice == "no")
-            {
-                return OnPostDontLogout();
-            }
-            else
-            {
-                return View("Logout");
-            }
-        }
-
-        [Authorize]
-        public IActionResult YourPage()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> OnPostLogoutAsync()
-        {
-            // ASP NET will handle all the signout functions for us
-            await _signInManager.SignOutAsync();
-
-            return RedirectToAction("Login");
-
-        }
-
-        [HttpPost]
-        public IActionResult OnPostDontLogout()
-        {
-            return RedirectToAction("YourPage");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginUserVM LoginUserVM)
-        {
-            if (ModelState.IsValid)
-            {
-                var identityResult = await _signInManager.PasswordSignInAsync(
-                    LoginUserVM.Email, LoginUserVM.Password, LoginUserVM.RememberMe, false);
-
-                if (identityResult.Succeeded)
-                {
-                    return RedirectToAction("YourPage");
-                }
-
-                ModelState.AddModelError("", "Username or Password is incorrect.");
+                return RedirectToAction("YourPage");
             }
 
-            return View(); // Return the Login view
+            ModelState.AddModelError("", "Username or Password is incorrect.");
         }
 
-        
+        return View(); // Return the Login view
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> RegisterUser(RegisterUserVM RegisterUserVM)
+    [HttpPost]
+    public async Task<IActionResult> RegisterUser(RegisterUserVM RegisterUserVM)
+    {
+        if (ModelState.IsValid)
         {
-            if (ModelState.IsValid)
+            // If ModelState is valid, create user
+            var user = new RallyUser()
             {
-                // If ModelState is valid, create user
-                var user = new RallyUser()
-                {
-                    UserName = RegisterUserVM.UserName,
-                    Email = RegisterUserVM.Email
-                };
+                UserName = RegisterUserVM.UserName,
+                Email = RegisterUserVM.Email
+            };
 
-                var result = await _userManager.CreateAsync(user, RegisterUserVM.Password);
+            var result = await _userManager.CreateAsync(user, RegisterUserVM.Password);
 
-                if (result.Succeeded)
-                {
-                    await _signInManager.SignInAsync(user, false);
-                    return View("Login");
-                }
-                // If errors in above print descriptions
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return View("Login");
             }
 
-            // her kan vi lave en fejl-view side og printe nogle beskeder ud, eventuelt
-            return RedirectToAction("Index", "Home");
-
+            // If errors in above print descriptions
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
         }
 
+        // her kan vi lave en fejl-view side og printe nogle beskeder ud, eventuelt
+        return RedirectToAction("Index", "Home");
     }
 }
