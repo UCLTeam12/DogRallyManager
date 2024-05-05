6625d1b9ddebe091aff3e640110b163be54e63d5
Michael Petersen <michael@smoerfugl.dk>
2024-05-05 14:18:56 +0200

    code cleanup remove unused code

--- DogRallyManager/DogRallyManager/Program.cs
+++ DogRallyManager/DogRallyManager/Program.cs
@@ -1,6 +1,5 @@
 using DogRallyManager.DbContexts;
 using DogRallyManager.Entities;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
