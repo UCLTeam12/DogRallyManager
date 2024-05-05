3bc07be1c4d302b17b18cfa94e01875f7d8516a8
Michael Petersen <michael@smoerfugl.dk>
2024-05-05 14:18:40 +0200

    fix linebreaks

--- DogRallyManager/DogRallyManager/Program.cs
+++ DogRallyManager/DogRallyManager/Program.cs
@@ -33,16 +33,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
 // @@@[FYT 3]
 app.UseAuthentication();
 app.UseAuthorization();
-
-
-
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
