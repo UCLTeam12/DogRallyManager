f24443dcce388640c61e9b08f00c171a12c9dbc4
Nicolai Storm <nmbk54072@edu.ucl.dk>
2024-05-09 15:57:10 +0200

    Added the include script from w3schools.com.

--- DogRallyManager/DogRallyManager/wwwroot/js/include.js
+++ DogRallyManager/DogRallyManager/wwwroot/js/include.js
@@ -1,5 +1,42 @@
+/* A function that searches an HTML file for the attribute include and includes the specified HTML file. This code snippet was
+   taken from w3schools.com. */
 
-async function IncludeHTML(file) {
-    const response = await fetch(file);
-    return response.text();
+function include() {
+	var z, i, elmnt, file, xhttp;
+
+	/* loop through a collection of all HTML elements: */
+
+	z = document.getElementsByTagName("*");
+	for (i = 0; i < z.length; i++) {
+		elmnt = z[i];
+
+		/* search for elements with a certain atrribute: */
+
+		file = elmnt.getAttribute("include");
+		if (file) {
+
+			/* make an HTTP request using the attribute value as the file name: */
+
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function () {
+				if (this.readyState == 4) {
+					if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+					if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+
+					/*remove the attribute, and call this function once more:*/
+
+					elmnt.removeAttribute("include");
+					include();
+				}
+			}
+			xhttp.open("GET", file, true);
+			xhttp.send();
+
+			/* exit the function: */
+
+			return;
+		}
+	}
 }
+
+/* JavaScript from w3schools. */
