ec5061b0d4a43a0177c2e4bfbf1ccb696e3ddcec
Michael Petersen <michael@smoerfugl.dk>
2024-05-14 14:43:13 +0200

    add sign migrations

---
+++ DogRallyManager/DogRallyManager/Migrations/20240514124105_Signs.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DogRallyManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class Signs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Signs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SignType = table.Column<string>(type: "TEXT", nullable: false),
+                    PositionX = table.Column<int>(type: "INTEGER", nullable: false),
+                    PositionY = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Signs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Signs");
+        }
+    }
+}
