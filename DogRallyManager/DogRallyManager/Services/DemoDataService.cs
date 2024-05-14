678ca7cc39a68d49d3fab41add18f8238fd25d1c
kitr4 <jeppeporsing@gmail.com>
2024-05-14 10:12:01 +0100

    Added a demo dataservice and a dataservice interface. Created chat controller and a respective demo view to test functionality.

---
+++ DogRallyManager/DogRallyManager/Services/DemoDataService.cs
@@ -0,0 +1,26 @@
+﻿using DogRallyManager.DbContexts;
+
+namespace DogRallyManager.Services
+{
+    public class DemoDataService : IDataService
+    {
+        private readonly DogRallyDbContext _dogRallyDbContext;
+
+        public DemoDataService(DogRallyDbContext dogRallyDbContext)
+        {
+            _dogRallyDbContext = dogRallyDbContext;
+        }
+
+        public async Task<List<Message>> GetAllMessagesAsync()
+        {
+            return _dogRallyDbContext.Messages.ToList();
+        }
+
+        public async Task AddMessageAsync(Message message)
+        {
+            await _dogRallyDbContext.Messages.AddAsync(message);
+            await _dogRallyDbContext.SaveChangesAsync();
+        }
+
+    }
+}
