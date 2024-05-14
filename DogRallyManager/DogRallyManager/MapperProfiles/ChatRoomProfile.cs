236b0ccea050a1b88ad332f140fadfe0298d0a3f
kitr4 <jeppeporsing@gmail.com>
2024-05-14 12:43:30 +0100

    Added AutoMapper profile for ChatRoomVM <-> ChatRoom entity. Also added MapperProfiles folder to contain these profiles.

---
+++ DogRallyManager/DogRallyManager/MapperProfiles/ChatRoomProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace DogRallyManager.MapperProfiles
+{
+    public class ChatRoomProfile : Profile
+    {
+        public ChatRoomProfile()
+        {
+            CreateMap<Entities.ChatRoom, ViewModels.ChatVMs.ChatRoomVM>();
+        }
+    }
+}
