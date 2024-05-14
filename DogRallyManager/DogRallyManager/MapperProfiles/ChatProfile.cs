fcc23de1b8cf269fb22658f59d3dfd298e09e9d0
kitr4 <jeppeporsing@gmail.com>
2024-05-14 14:57:45 +0100

    Did some stuff. Chat view now accepts a ChatViewModel as a wrapper model containing a list of chatrooms and chatmessages. Also deleted mapper profiles, and inserted a new general one for the chat: ChatProfile

---
+++ DogRallyManager/DogRallyManager/MapperProfiles/ChatProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DogRallyManager.Entities;
+using DogRallyManager.ViewModels.ChatVMs;
+
+namespace DogRallyManager.MapperProfiles
+{
+    public class ChatProfile : Profile
+    {
+        public ChatProfile()
+        {
+            CreateMap<ChatRoom, ChatRoomVM>();
+            CreateMap<Message, ChatMessageVM>();
+            CreateMap<ChatRoomVM, ChatRoom>();
+            CreateMap<ChatMessageVM, Message>();
+        }
+    }
+}
---
+++ DogRallyManager/DogRallyManager/ViewModels/ChatVMs/ChatViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DogRallyManager.ViewModels.ChatVMs
+{
+    public class ChatViewModel
+    {
+        // THIS IS A WRAPPER CLASS
+        public IEnumerable<ChatRoomVM> ChatRooms { get; set; }
+        public IEnumerable<ChatMessageVM> ChatMessages { get; set; } 
+    }
+}
