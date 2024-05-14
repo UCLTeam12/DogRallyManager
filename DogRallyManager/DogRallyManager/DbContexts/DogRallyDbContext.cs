57a374de15e797730b54580893d6d6f908ae35f2
Michael Petersen <michael@smoerfugl.dk>
2024-05-14 14:42:59 +0200

    add sign to the database

--- DogRallyManager/DogRallyManager/DbContexts/DogRallyDbContext.cs
+++ DogRallyManager/DogRallyManager/DbContexts/DogRallyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using DogRallyManager.Entities;
+﻿using DogRallyManager.Database.Models.Signs;
+using DogRallyManager.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,10 +8,10 @@ namespace DogRallyManager.DbContexts
     public class DogRallyDbContext : IdentityDbContext
     {
         public DbSet<RallyUser> RallyUsers { get; set; } 
-
         public DbSet<RallyUserRole> RallyUserRoles { get; set; }
         public DbSet<ChatRoom> ChatRooms { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<Sign> Signs { get; set; }
 
         public DogRallyDbContext(DbContextOptions<DogRallyDbContext> options) : base(options)
         {
