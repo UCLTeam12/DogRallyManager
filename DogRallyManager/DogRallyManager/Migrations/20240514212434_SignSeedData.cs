e658904f3ec125e506635cb0719cc75e9d83c726
Michael Petersen <michael@smoerfugl.dk>
2024-05-14 23:47:06 +0200

    seed data snapshot and migrations

---
+++ DogRallyManager/DogRallyManager/Migrations/20240514212434_SignSeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DogRallyManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class SignSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Signs",
+                columns: new[] { "Id", "PositionX", "PositionY", "SignType" },
+                values: new object[,]
+                {
+                    { 1, 50, 50, "exercise-1.png" },
+                    { 2, 100, 100, "exercise-2.png" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Signs",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Signs",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
