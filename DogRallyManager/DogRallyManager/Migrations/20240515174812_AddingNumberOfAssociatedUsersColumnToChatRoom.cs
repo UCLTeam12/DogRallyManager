77c48dd4c0519b8a201edfc4adb29c6a64b22368
Michael Petersen <michael@smoerfugl.dk>
2024-05-16 10:57:43 +0200

    add migrations

---
+++ DogRallyManager/DogRallyManager/Migrations/20240515174812_AddingNumberOfAssociatedUsersColumnToChatRoom.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DogRallyManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingNumberOfAssociatedUsersColumnToChatRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfAssociatedUsers",
+                table: "ChatRooms",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfAssociatedUsers",
+                table: "ChatRooms");
+        }
+    }
+}
