86056dff43d52daf6fd0976cd20143efff4b7761
Michael Petersen <michael@smoerfugl.dk>
2024-05-16 10:30:49 +0200

    remove chatVMs

--- DogRallyManager/DogRallyManager/Services/IMessageCreationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using DogRallyManager.ViewModels.ChatVMs;
-
-namespace DogRallyManager.Services
-{
-    public interface IMessageCreationService
-    {
-        Task CreateMessageAsync(ChatMessageVM chatMessageVM, ChatRoomVM chatRoom);
-    }
-}
