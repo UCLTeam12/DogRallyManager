e1db02c87f10656d67cce4d338a7c0ef47b47a9f
Michael Petersen <michael@smoerfugl.dk>
2024-05-16 10:58:12 +0200

    add migrations

---
+++ DogRallyManager/DogRallyManager/Migrations/20240515181454_AddTriggersToUpdateChatRoomsAssociatedUsersWorks.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DogRallyManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTriggersToUpdateChatRoomsAssociatedUsersWorks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE TRIGGER UpdateNumberOfAssociatedUsersOnUserAdd
+                AFTER INSERT ON ChatRoomRallyUser
+                BEGIN
+                    UPDATE ChatRooms
+                    SET NumberOfAssociatedUsers = (
+                        SELECT COUNT(*) FROM ChatRoomRallyUser WHERE UserId = NEW.UserId
+                    )
+                WHERE Id = NEW.UserId;
+            END;
+            ");
+
+            migrationBuilder.Sql(@"
+            CREATE TRIGGER UpdateNumberOfAssociatedUsersOnUserDelete
+            AFTER DELETE ON ChatRoomRallyUser
+            BEGIN
+                UPDATE ChatRooms
+                SET NumberOfAssociatedUsers = (
+                    SELECT COUNT(*) FROM ChatRoomRallyUser WHERE UserId = OLD.UserId
+                )
+                WHERE Id = OLD.UserId;
+            END;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER UpdateNumberOfAssociatedUsersOnUserAdd");
+            migrationBuilder.Sql("DROP TRIGGER UpdateNumberOfAssociatedUsersOnUserDelete");
+        }
+    }
+}
