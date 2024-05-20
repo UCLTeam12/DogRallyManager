c39c18a9766649424f1815f31b8fc5ca6036d901
kitr4 <jeppeporsing@gmail.com>
2024-05-20 09:27:30 +0100

    SearchUser functionality

--- DogRallyManager/DogRallyManager/Controllers/SearchUserController.cs
+++ DogRallyManager/DogRallyManager/Controllers/SearchUserController.cs
@@ -7,22 +7,29 @@ namespace DogRallyManager.Controllers
 {
     public class SearchUserController : Controller
     {
-        private readonly IDataService _dataservice;
+        private readonly IDataService _dataService;
         private readonly IMapper _mapper;
 
         public SearchUserController(IDataService dataservice, 
             IMapper mapper)
         {
-            _dataservice = dataservice;
+            _dataService = dataservice;
             _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
-            var ListOfUserVMs = await _dataservice.GetAllUserNamesAsync();
-
-            return View(ListOfUserVMs);
+            return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchUser(string username)
+        {
+            var users = await _dataService.GetUserAsync(username);
+            var userViewModels = _mapper.Map<List<UserViewModel>>(users);
+            return Json(userViewModels);
+        }
+
         [HttpGet]
         public IActionResult StartChat(string recipientUsername)
         {
