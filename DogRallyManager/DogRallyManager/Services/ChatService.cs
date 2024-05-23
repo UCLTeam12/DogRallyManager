89877ded4d8bf1a481ee43a63a802079d0b0fe33
kitr4 <jeppeporsing@gmail.com>
2024-05-23 19:09:54 +0100

    Extracted more methods to ChatService

--- DogRallyManager/DogRallyManager/Services/ChatService.cs
+++ DogRallyManager/DogRallyManager/Services/ChatService.cs
@@ -1,5 +1,6 @@
-﻿using DogRallyManager.DbContexts;
+﻿using AutoMapper;
 using DogRallyManager.Entities;
+using DogRallyManager.ViewModels.ChatVMs;
 using Microsoft.AspNetCore.Identity;
 
 namespace DogRallyManager.Services
@@ -8,12 +9,15 @@ namespace DogRallyManager.Services
     {
         private readonly UserManager<RallyUser> _userManager;
         private readonly IDataService _dataService;
+        private readonly IMapper _mapper;
 
-        public ChatService(UserManager<RallyUser> usermanager, 
-            IDataService dataService)
+        public ChatService(UserManager<RallyUser> usermanager,
+            IDataService dataService,
+            IMapper mapper)
         {
             _userManager = usermanager;
             _dataService = dataService;
+            _mapper = mapper;
         }
 
         public async Task CreateGeneralChatRoomAsync()
@@ -26,5 +30,20 @@ namespace DogRallyManager.Services
                 await _dataService.CreateChatRoom(createdGeneralRoom);
             }
         }
+
+        public string GetRoomName(string userName1, string userName2)
+        {
+            // Sorting it alphabethically
+            var sortedUsernames = new List<string> { userName1, userName2 }.OrderBy(u => u).ToList();
+            return $"Chatroom: {sortedUsernames[0]} and {sortedUsernames[2]}";
+        }
+
+        public async Task<ChatRoomVM> GetUserAssociatedChatRoomsVM(string userId)
+        {
+            var entityChatRooms = await _dataService.GetUserAssociatedChatRoomsWithMessagesAsync(userId);
+            var chatRoomsVM = _mapper.Map<ChatRoomVM>(entityChatRooms);
+            return chatRoomsVM;
+        }
+
     }
 }
