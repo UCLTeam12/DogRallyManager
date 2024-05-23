0447198650ffca992cb50cb6942e31066e678a81
Michael Petersen <michael@smoerfugl.dk>
2024-05-24 00:23:10 +0200

    add dbset to Boards

--- DogRallyManager/DogRallyManager/DbContexts/DogRallyDbContext.cs
+++ DogRallyManager/DogRallyManager/DbContexts/DogRallyDbContext.cs
@@ -1,4 +1,5 @@
-﻿using DogRallyManager.Database.Models.Signs;
+﻿using DogRallyManager.Database.Models.Boards;
+using DogRallyManager.Database.Models.Signs;
 using DogRallyManager.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -12,7 +13,7 @@ namespace DogRallyManager.DbContexts
         public DbSet<ChatRoom> ChatRooms { get; set; }
         public DbSet<Message> Messages { get; set; }
         public DbSet<Sign> Signs { get; set; }
-
+        public DbSet<Board> Boards { get; set; }
         public DogRallyDbContext(DbContextOptions<DogRallyDbContext> options) : base(options)
         {
             
