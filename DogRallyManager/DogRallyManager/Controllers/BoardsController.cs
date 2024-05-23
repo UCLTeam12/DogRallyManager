25f0558808281e74daaaff3c0f5c97cd0079c50e
Michael Petersen <michael@smoerfugl.dk>
2024-05-24 00:31:33 +0200

    Add CreateBoard

--- DogRallyManager/DogRallyManager/Controllers/BoardsController.cs
+++ DogRallyManager/DogRallyManager/Controllers/BoardsController.cs
@@ -1,3 +1,5 @@
+using DogRallyManager.Database.Models.Boards;
+using DogRallyManager.Database.Models.Signs;
 using DogRallyManager.DbContexts;
 using DogRallyManager.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -6,13 +8,51 @@ namespace DogRallyManager.Controllers;
 
 public class BoardsController(DogRallyDbContext dbContext) : Controller
 {
-    public IActionResult Index()
+    public record BoardCreateModel(string name);
+    public IActionResult Details(int id)
     {
+        var board = dbContext.Boards.Find(id);
         var boardModel = new BoardModel
         {
-            Signs = dbContext.Signs.ToList()
+            Signs = dbContext.Signs.ToList(),
+            Id = board.Id
         };
         return View(boardModel);
     }
+
+    [HttpPost]
+    public IActionResult CreateBoard([FromForm]BoardCreateModel boardCreateModel)
+    {
+        var signList = new List<Sign>();
+        for (int i = 1; i < 300; i++)
+        {
+            signList.Add(
+
+                new Sign()
+                {
+                    SignType = "exercise-"+i+".png"
+                }
+            );
+
+        }
+        var board = new Board()
+        {
+            Name = boardCreateModel.name,
+            Sign = signList,
+        };
+        dbContext.Boards.Add(board);
+        dbContext.SaveChanges();
+        return RedirectToAction("Details", new {id= board.Id});
+    }
+    public IActionResult Index()
+    {
+        var boards = dbContext.Boards.ToList();
+        var pageModel = new DogRallyManager.Views.Boards.Index
+        {
+            BoardsList = boards
+        };
+        return View(pageModel);
+    }
+    
     
 }
