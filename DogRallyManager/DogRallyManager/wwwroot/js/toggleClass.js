2d2054e346f57ba32f07ffcfe5c3bf2c4b1e9ebd
kitr4 <jeppeporsing@gmail.com>
2024-05-25 21:55:54 +0100

    Add user to board function next up: implement search user as a component, window that pops up in the middle of the screen

--- DogRallyManager/DogRallyManager/wwwroot/js/toggleClass.js
+++ DogRallyManager/DogRallyManager/wwwroot/js/toggleClass.js
@@ -10,5 +10,8 @@ function toggleClass(id, class1, class2) {
 
         element.classList.remove(class1);
         element.classList.add(class2);
+    } else {
+        element.classList.remove(class2);
+        element.classList.add(class1);
     }
 }
