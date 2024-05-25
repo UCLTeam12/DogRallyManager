8946bc7f7e9503ab78781390f6a13d961e74c037
kitr4 <jeppeporsing@gmail.com>
2024-05-25 17:04:34 +0100

    Boards and user association, dependency injjections on boards controller

--- DogRallyManager/DogRallyManager/Database/Models/Boards/Board.cs
+++ DogRallyManager/DogRallyManager/Database/Models/Boards/Board.cs
@@ -6,7 +6,6 @@ namespace DogRallyManager.Database.Models.Boards;
 
 public class Board
 {
-    
     public int Id { get; set; }
     public string Name { get; set; }
     public ICollection<Sign> Sign { get; set; } = [];
