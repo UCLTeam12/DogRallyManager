97da2f10d7049a23bdee6de36a18499e70093341
Nicolai Storm <nmbk54072@edu.ucl.dk>
2024-05-25 21:38:51 +0200

    Updated the toggleClass function, so that it can now be used to minimize the chat.

--- DogRallyManager/DogRallyManager/wwwroot/js/toggleClass.js
+++ DogRallyManager/DogRallyManager/wwwroot/js/toggleClass.js
@@ -1,20 +1,18 @@
 
-/* Script that toggles between two classes. */
+/* A function that takes an id and two classes as parameters
+   and toggles between the two classes in the element with
+   the given id when called. */
 function toggleClass(id, class1, class2) {
 
     const element = document.getElementById(id);
 
-    // If element contains "class1", remove "class1" and add "class1".
     if (element.classList.contains(class1)) {
-        
+
         element.classList.remove(class1);
         element.classList.add(class2);
     }
-
-    //// If element contains "class1", remove "class2" and add "class1".
-    //else if (element.classList.contains(class2)) {
-
-    //    element.classList.remove(class2);
-    //    element.classList.add(class1);
-    //}
+    else if (element.classList.contains(class2)) {
+        element.classList.remove(class2);
+        element.classList.add(class1);
+    }
 }
