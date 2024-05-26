74dda791c18f1237b0f6b5101bfacb46accfe858
kitr4 <jeppeporsing@gmail.com>
2024-05-26 18:25:45 +0100

    SearchComponent now retrieves users, and functionality for adding them to the board works.

--- DogRallyManager/DogRallyManager/Services/ISearchService.cs
+++ DogRallyManager/DogRallyManager/Services/ISearchService.cs
@@ -5,5 +5,6 @@ namespace DogRallyManager.Services
     public interface ISearchService
     {
         Task<List<UserViewModel>> SearchUser(string userName);
+        Task<bool> AddUserToBoardAsync(string username, int boardId);
     }
 }
