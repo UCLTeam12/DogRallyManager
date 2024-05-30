f2a0eef31e291089267cd5a5c9cd82de09c9e2e1
Michael Petersen <michael@smoerfugl.dk>
2024-05-30 21:52:41 +0200

    Add seed to db for admin role

---
+++ DogRallyManager/DogRallyManager/Migrations/20240530195015_AdminRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DogRallyManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Discriminator", "Name", "NormalizedName" },
+                values: new object[] { "495ad32f-2d1d-49b9-976c-f6e6d1755418", null, "RallyUserRole", "Admin", "ADMIN" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "495ad32f-2d1d-49b9-976c-f6e6d1755418");
+        }
+    }
+}
